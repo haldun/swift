21bbc385e9fe840171e95e20260ebed5c9fd65ba
usama <u_hameed@apple.com>
2024-10-17 15:16:39 -0700

    Push assertions in the deserializer

--- lib/Serialization/DeserializeSIL.cpp
+++ lib/Serialization/DeserializeSIL.cpp
@@ -3658,10 +3658,11 @@ SILFunction *SILDeserializer::lookupSILFunction(SILFunction *InFunc,
     return nullptr;
   }
 
-  if (maybeFunc.get()) {
+  if (auto fn = maybeFunc.get()) {
     LLVM_DEBUG(llvm::dbgs() << "Deserialize SIL:\n";
                maybeFunc.get()->dump());
     assert(InFunc->getName() == maybeFunc.get()->getName());
+    assert(!fn->isZombie());
   }
 
   return maybeFunc.get();
@@ -3748,6 +3749,9 @@ bool SILDeserializer::hasSILFunction(StringRef Name,
     return false;
   }
 
+  if (onlyReferencedByDebugInfo)
+    return false;
+
   // Bail if it is not a required linkage.
   if (Linkage && linkage.value() != *Linkage)
     return false;
@@ -3773,9 +3777,10 @@ SILFunction *SILDeserializer::lookupSILFunction(StringRef name,
     return nullptr;
   }
 
-  if (maybeFunc.get()) {
+  if (auto fn = maybeFunc.get()) {
     LLVM_DEBUG(llvm::dbgs() << "Deserialize SIL:\n";
                maybeFunc.get()->dump());
+    assert(!fn->isZombie());
   }
   return maybeFunc.get();
 }
